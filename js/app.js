ae5db87ff1fea44e3e9915cd14f4e9ff8d6cc8ea
ThaliaDiaz <thalidiaz24@gmail.com>
2017-10-10 23:32:23 -0500

    version fin

--- js/app.js
+++ js/app.js
@@ -9,14 +9,12 @@ if(numCard.length==0 || typeof(numCard)==='NaN'|| numCard==' '){
 }
 //Creo mi funcion para validar la tarjeta de acuerdo al número ingresado
 var isValidCard= function(numCard){
-//declaración de mis  variables
-    //convertir el número ingresado en una cadena
-     var str=numCard.toString();
+    //declaración de mis  variables
     //me sirve para separar cada digito de el numero que en este caso esta en cadena y mostrarlo en un array
-     var arr=str.split('');
+     var arr=numCard.split('');
     //invierte el array
-     arr.reverse();
-     parseInt(arr.reverse());
+     var arrInvertido=arr.reverse();
+     console.log('este es el reverso: '+arrInvertido);
     //Inicializo las variables que luego utilizaré
      var prod=1;
      var sum=0;
@@ -25,20 +23,21 @@ var isValidCard= function(numCard){
        //Determino si una posición es par
          if(i%2===0){
            //Asigno a prod el valor del número en la posición i multiplicada por dos
-             prod=parseInt(arr[i]*2);
+             prod=arr[i]*2;
            //Si el prod es menor que 10 entra y se asigna ese valor en reemplazo de el elemento que esta en la posicion i del arreglo
              if(prod<10){
                  arr[i]=prod;
              }else{
                   // si no se cumplió la condición anterior entonces prod>=10 por lo tanto realizo la operación de acuerdo al
-                 // algoritmo de luhn
+                 // algoritmo de luhn, que en este caso es sumar las cifras del prod
                  resul=Math.floor(prod/10)+(prod%10);
                  // Asigno el valor de mi resul al elemento en la posicion i del array
                  arr[i]=resul;
              }
          }
          else{
-           parseInt(arr[i]);
+           //si la posicion es impar solo lo mulplico por 1 para convertirlo a tipo de dato numerico y escribo el mismo numero
+           arr[i]=arr[i]*1;
          }
      }
      //muestro mi nuevo arreglo
@@ -48,7 +47,7 @@ var isValidCard= function(numCard){
        sum=sum+arr[i];
      }
      //muestro mi suma total
-     //console.log(sum);
+     console.log(sum);
      //Pregunta si la suma del arreglo actual es divisible entre 10
      if(sum%10===0){
         //Si es divisible muestra que es un  tarjeta válida.
@@ -61,4 +60,5 @@ var isValidCard= function(numCard){
   return msg;
 }
 
- document.write(isValidCard(numCard));
+ //document.write(isValidCard(numCard));
+ console.log(isValidCard(numCard));
